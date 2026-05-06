5e4ebffe9b2dbb67a331fa3f1faf803e334f20f8
saranaga <sarangasama@gmail.com>
2026-05-07 05:17:59 +0530

    Refactor resume and project details; remove unused assets and styles
    - Updated resume content to reflect current skills and experience, including a new summary and work experience details.
    - Removed unused SVG icons and images from the assets folder.
    - Deleted unnecessary CSS file and cleaned up imports in App.jsx.
    - Refactored Navbar component for improved readability and structure.
    - Adjusted Vite configuration for cleaner plugin usage and resolved path aliases.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,39 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
-import { navIcons, navLinks } from '#constants'
-import React from 'react'
+import React from "react";
+import { navLinks, navIcons } from "#constants";
+
 
 const Navbar = () => {
-
-
-
   return (
     <nav>
-        <div>
-            <img src="/images/logo.svg" alt="logo" />
-            <p className='font-bold'>Saranga's Portfolio</p>
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-bold">Saranga's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id} className="hover:underline">
+              {name}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-            <ul>
-                {navLinks.map(({id, name}) => (
-                    <li key={id}>
-                        <p>{name}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div>
+        <ul>
+          {
+            navIcons.map(({id, img}) => (
+              <li key={id}>
+                <img src={img} alt={`icon-${id}`} className="icon-hover" />
+              </li>
+            ))
+          }
+        </ul>
 
-        <div>
-            <ul>
-                {navIcons.map(({id, img}) => (
-                    <li key={id}>
-                        <img src={img} className='icon-hover' alt={`icon-${id}`} />
-                    </li>
-                ))}
-            </ul>
-
-            <time>{dayjs().format('ddd MMM D h:mm A')}</time>
-
-        </div>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
