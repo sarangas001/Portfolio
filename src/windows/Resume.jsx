417c4817f65a4a4af2c5ddf244e5ce85530a39ae
saranaga <sarangasama@gmail.com>
2026-05-16 09:44:32 +0530

    feat: add Resume window with PDF viewer and download functionality

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import WindowWrapper from '../hoc/WindowWrapper'
-import { WindowControlls } from '../components'
+import React from "react";
+import WindowWrapper from "../hoc/WindowWrapper";
+import { WindowControlls } from "../components";
+import { DownloadIcon } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
 
 const Resume = () => {
   return (
     <>
-        <div id="window-header">
-            <WindowControlls target="resume" />
-            <h2>Resume.pdf</h2>
-        </div>
-        
+      <div id="window-header">
+        <WindowControlls target="resume" />
+        <h2>Resume.pdf</h2>
+        <a
+          href="files/resume.pdf"
+          download
+          className="cursor-pointer"
+          title="Download Resume"
+        >
+          <DownloadIcon className="icon" />
+        </a>
+      </div>
+      <Document file="files/resume.pdf">
+        <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+      </Document>
     </>
-  )
-}
+  );
+};
 
-const ResumeWindow = WindowWrapper(Resume, 'resume')
+const ResumeWindow = WindowWrapper(Resume, "resume");
 
 export default ResumeWindow;
